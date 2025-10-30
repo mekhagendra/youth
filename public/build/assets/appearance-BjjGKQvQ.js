08ce3f1ca64292eff23e025ee177396d3c02d938
Khagendra Neupane <kneupane32@gmail.com>
2025-10-30 17:35:13 +1100

    update

---
+++ public/build/assets/appearance-BjjGKQvQ.js
@@ -0,0 +1,16 @@
+import{j as e}from"./ui-C7Y-3-W8.js";import{b as m,H as d}from"./app-C15jCSql.js";import{c as r}from"./button-DgatVAxH.js";import{c as a}from"./createLucideIcon-BB07dq5o.js";import{e as h,S as u,H as x}from"./layout-CLAiF5lc.js";import{A as y}from"./app-layout-CEsnrBER.js";import"./vendor-CjZsb-BP.js";import"./app-BYxF2paC.js";import"./app-logo-icon-D1vmkisE.js";import"./index-acbBlXq8.js";import"./index-UUvH3jlQ.js";import"./index-Vk_PNSMy.js";import"./x-LD60btdu.js";import"./chevron-right-DfKatPhh.js";/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const k=[["rect",{width:"20",height:"14",x:"2",y:"3",rx:"2",key:"48i651"}],["line",{x1:"8",x2:"16",y1:"21",y2:"21",key:"1svkeh"}],["line",{x1:"12",x2:"12",y1:"17",y2:"21",key:"vw1qmm"}]],g=a("Monitor",k);/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const b=[["path",{d:"M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z",key:"a7tn18"}]],f=a("Moon",b);/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const j=[["circle",{cx:"12",cy:"12",r:"4",key:"4exip2"}],["path",{d:"M12 2v2",key:"tus03m"}],["path",{d:"M12 20v2",key:"1lh1kg"}],["path",{d:"m4.93 4.93 1.41 1.41",key:"149t6j"}],["path",{d:"m17.66 17.66 1.41 1.41",key:"ptbguv"}],["path",{d:"M2 12h2",key:"1t8f8n"}],["path",{d:"M20 12h2",key:"1q8mjw"}],["path",{d:"m6.34 17.66-1.41 1.41",key:"1m8zz5"}],["path",{d:"m19.07 4.93-1.41 1.41",key:"1shlcs"}]],v=a("Sun",j);function A({className:n="",...s}){const{appearance:o,updateAppearance:i}=m(),c=[{value:"light",icon:v,label:"Light"},{value:"dark",icon:f,label:"Dark"},{value:"system",icon:g,label:"System"}];return e.jsx("div",{className:r("inline-flex gap-1 rounded-lg bg-neutral-100 p-1 dark:bg-neutral-800",n),...s,children:c.map(({value:t,icon:p,label:l})=>e.jsxs("button",{onClick:()=>i(t),className:r("flex items-center rounded-md px-3.5 py-1.5 transition-colors",o===t?"bg-white shadow-xs dark:bg-neutral-700 dark:text-neutral-100":"text-neutral-500 hover:bg-neutral-200/60 hover:text-black dark:text-neutral-400 dark:hover:bg-neutral-700/60"),children:[e.jsx(p,{className:"-ml-1 h-4 w-4"}),e.jsx("span",{className:"ml-1.5 text-sm",children:l})]},t))})}const M=[{title:"Appearance settings",href:h().url}];function R(){return e.jsxs(y,{breadcrumbs:M,children:[e.jsx(d,{title:"Appearance settings"}),e.jsx(u,{children:e.jsxs("div",{className:"space-y-6",children:[e.jsx(x,{title:"Appearance settings",description:"Update your account's appearance settings"}),e.jsx(A,{})]})})]})}export{R as default};
